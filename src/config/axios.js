bccfcef114a7caf0cc7b04c7e31216cc6360e23f
Diana <dianamorenoarranz@gmail.com>
2021-08-25 15:40:25 +0200

    add server to production

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,19 @@
-import axios from 'axios'
+import axios from 'axios';
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:4000/'
-})
+  baseURL: isLocalhost
+    ? 'http://localhost:4000/'
+    : 'https://crud-redux-server-1.herokuapp.com/',
+});
 
-export default axiosClient
+export default axiosClient;
