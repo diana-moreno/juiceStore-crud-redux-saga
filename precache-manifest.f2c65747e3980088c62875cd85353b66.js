e7e03bbe6c886ffb4643277305766e6d4f599269
Diana <diana.moreno@interficie.com>
2023-08-06 12:57:04 +0200

    Updates

---
+++ precache-manifest.f2c65747e3980088c62875cd85353b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec06e4eab2a28115aa023514b7986ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1a6177d1017cab89ec",
+    "url": "/static/css/main.435e5ce2.chunk.css"
+  },
+  {
+    "revision": "ab0f5639e91481b502fa",
+    "url": "/static/js/2.73cdd8ba.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.73cdd8ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a1a6177d1017cab89ec",
+    "url": "/static/js/main.131f64c8.chunk.js"
+  },
+  {
+    "revision": "11e489c29f4b1c76f609",
+    "url": "/static/js/runtime-main.f8c5c3f7.js"
+  },
+  {
+    "revision": "11a2189e69c59d2973a9dbfee3b000a4",
+    "url": "/static/media/pineapple-2588302_1920.11a2189e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3a2715666069ccb294a770c9a13e450.js"
+  "/precache-manifest.f2c65747e3980088c62875cd85353b66.js"
 );
 
 self.addEventListener('message', (event) => {
